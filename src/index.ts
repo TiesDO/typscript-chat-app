be046422fefbb8feac6ba51eb3bb7711e375f23c
TiesDO <denouden.ties@gmail.com>
2023-02-13 00:59:33 +0100

    send message from client
    - server starts
    - server sends message
    - server recieves message
    - client connects
    - client sends message
    - client recieves message
    
    client code in this case is the connectSocket.html

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,27 @@
 import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
 
+// have express run on the http server
 const app = express();
+const server = http.createServer(app);
 
 app.get('/', (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(4000, () => {
-    console.log('express-server listening on: 127.0.0.1:4000');
+// setup websocket on the same server (websocket has a different protocol then http so there is no conflict)
+const wss = new WebSocket.Server({ server: server});
+
+// setup ws events
+wss.on('connection', (ws) => {
+    ws.on('message', (msg) => {
+        console.log("recieved: %s", msg);
+    });
+
+    ws.send("hello client");
 })
+
+server.listen(4000, () => {
+    console.log('express-server listening on: 127.0.0.1:4000');
+});
