33c605a40cf37ff2348f80f15d1432a2b097de11
TiesDO <denouden.ties@gmail.com>
2023-02-13 04:43:08 +0100

    add global chat
    #server
    
    - broadcast message function
    - client id's
    
    #client
    
    - show incomming messages (server and others)
    - show outgoing messages

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,85 @@
 import express from 'express';
 import http from 'http';
-import WebSocket from 'ws';
+import WebSocket, { CloseEvent, ErrorEvent, MessageEvent } from 'ws';
+import { v4 } from 'uuid';
 
-// have express run on the http server
 const app = express();
 const server = http.createServer(app);
 
-app.get('/', (req, res) => {
-    res.send("Hello, World!");
-});
-
-// setup websocket on the same server (websocket has a different protocol then http so there is no conflict)
 const wss = new WebSocket.Server({ server: server});
 
-// setup ws events
-wss.on('connection', (ws) => {
-    ws.on('message', (msg) => {
-        console.log("recieved: %s", msg);
+const SERVER_UUID = "host_server";
+
+interface ClientSocket extends WebSocket {
+    uuid: string;
+}
+
+interface Client {
+    socket: ClientSocket;
+    joined: Date;
+    uuid: string;
+}
+
+interface Message {
+    data: string;
+    senderID: string;
+}
+
+// keep track of all connected clients
+let connected: Client[] = [];
+
+wss.on('connection', (ws: ClientSocket) => {
+    ws.on('message', (ev: MessageEvent) => {
+        BroadcastMessage({
+            data: String(ev),
+            senderID: ws.uuid,
+        }, [ws.uuid]);
     });
 
-    ws.send("hello client");
+    ws.on('error', (ev: ErrorEvent) => {
+        console.error(ev.message);
+    });
+
+    ws.on('close', (ev: CloseEvent) => {
+        DisconnectClient(ws);
+    })
+
+
+    ConnectNew(ws);
 })
 
+function ConnectNew(client: ClientSocket) {
+    client.uuid = v4();
+
+    connected.push({
+        uuid: client.uuid,
+        joined: new Date(Date.now()),
+        socket: client,
+    });
+
+    BroadcastMessage({
+        data: `${client.uuid} has joined the server`,
+        senderID: SERVER_UUID,
+    }, [client.uuid]);
+}
+
+function DisconnectClient(client: ClientSocket) {
+    BroadcastMessage({
+        data: `${client.uuid} has left the server`,
+        senderID: SERVER_UUID,
+    }, [client.uuid]);
+
+    delete connected[client.uuid];
+}
+
+function BroadcastMessage(message: Message, exceptions: string[]) {
+    connected.forEach(c => {
+        if (exceptions.includes(c.uuid)) { return }
+        
+        c.socket.send(JSON.stringify(message));
+    })
+}
+
 server.listen(4000, () => {
-    console.log('express-server listening on: 127.0.0.1:4000');
+    console.log('chat-server listening on: 127.0.0.1:4000');
 });
